4e40c25583b8f30c0e8cd61c87eca26459dbec76
oloyedev <104859747+oloyedev@users.noreply.github.com>
2024-06-03 09:37:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 from flask import Flask, render_template, request
 import numpy as np
-import base64
 import cv2
 import os
 
 from ultralytics import YOLO
-model = YOLO('C:/Users/Oloye/Documents/NB/NB_PI/best.pt')
+model = YOLO('C:/Users/Oloye/Desktop/Work/best (1).pt')
 
 extensions = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 
+class_map = {
+    0: 'Amstel Malt Bottle',
+    1: 'Amstel Malt Can',
+    2: 'Desperados',
+    3: 'Heineken'
+}
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in extensions
 
 def predict_on_image(image_stream):
-    image = cv2.imdecode( np.asarray(bytearray(image_stream.read()), dtype=np.uint8) , cv2.IMREAD_COLOR)
-
+    image = cv2.imdecode(np.asarray(bytearray(image_stream.read()), dtype=np.uint8), cv2.IMREAD_COLOR)
     results = model.predict(image, conf=0.5)
-    for i, r in enumerate(results):
-        im_bgr = r.plot(conf=False)
 
-    return im_bgr
+    predictions = []
+    for r in results:
+        for box in r.boxes:
+            class_id = int(box.cls.item())  # Convert tensor to int
+            confidence = float(box.conf.item())  # Convert tensor to float
+            class_name = class_map.get(class_id, "Unknown")
+            predictions.append((class_name, confidence))
+
+    return predictions
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -35,21 +46,11 @@ def home():
             return render_template('index.html', error='No selected file')
 
         if file and allowed_file(file.filename):
-
-            predicted_image = predict_on_image(file.stream)
-
-            retval, buffer = cv2.imencode('.png', predicted_image)
-            detection_img_base64 = base64.b64encode(buffer).decode('utf-8')
-
-            file.stream.seek(0)
-            original_img_base64 = base64.b64encode(file.stream.read()).decode('utf-8')
-
-            return render_template('result.html', original_img_data=original_img_base64, detection_img_data=detection_img_base64)
+            predictions = predict_on_image(file.stream)
+            return render_template('result.html', predictions=predictions)
 
     return render_template('index.html')
 
 if __name__ == '__main__':
     os.environ.setdefault('FLASK_ENV', 'development')
     app.run(debug=False, port=5000, host='0.0.0.0')
-
-
